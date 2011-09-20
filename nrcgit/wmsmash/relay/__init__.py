1a5bbf53cdebb46908a7bdc11dd1d4c5e4caae10
Ivan Boldyrev <lispnik@gmail.com>
2011-09-20 21:00:09 +0700

    Change SQL queries to use tables from Django editor.

--- nrcgit/wmsmash/relay/__init__.py
+++ nrcgit/wmsmash/relay/__init__.py
@@ -32,19 +32,25 @@ describes layerset, second one is a list of layers.
 If layerset does not exists, None is returned."""
     # Get layerset info: name, title, abastract, author name
     lset = yield dbpool.runQuery(
-"""SELECT layerset.name, title, abstract, users.username FROM layerset JOIN users ON users.id = layerset.author_id WHERE layerset.name = %s
+"""SELECT editor_set.name, title, abstract, auth_user.username FROM editor_set JOIN auth_user ON auth_user.id = editor_set.author_id WHERE editor_set.name = %s
 """, (set_name,))
     # Fetch layers in the layerset and return a tuple (layersetData, layerdata)
     if lset:
+        # TODO visible and public fields too, as we need them to
+        # reconstruct whole tree.
         layers = yield dbpool.runQuery(
-"""SELECT layertree.id, layertree.name, layers.title, layers.abstract,
-          layers.name, servers.url, layertree.parent_id, layertree.parent_id,
-          layertree.ord, layers.latlngbb, layers.capabilites
-  FROM layertree JOIN layerset ON layertree.lset_id = layerset.id
-    LEFT JOIN layers ON layertree.layer_id = layers.id
-    LEFT JOIN servers ON layers.server_id = servers.id
-  WHERE layerset.name = %s AND NOT layertree.hidden AND (layers.available OR layers.available IS NULL)
-ORDER BY parent_id ASC, ord DESC""", (set_name,))
+"""SELECT editor_layertree.id, editor_namedlayertree.name,
+          editor_layer.title, editor_layer.abstract, editor_namedlayer.name,
+          editor_server.url,
+          editor_layertree.parent_id, editor_layertree.first_id, editor_layertree.nxt_id,
+          editor_layer.latlngbb, editor_layer.capablilities
+  FROM editor_layertree JOIN editor_set ON editor_layertree.lset_id = editor_set.id
+    LEFT JOIN editor_layer ON editor_layertree.layer_id = editor_layer.id
+    LEFT JOIN editor_server ON editor_layer.server_id = editor_server.id
+    LEFT JOIN editor_namedlayertree ON editor_namedlayertree.id = editor_layertree.named_id
+    LEFT JOIN editor_namedlayer ON editor_namedlayer.id = editor_layer.named_id
+  WHERE editor_set.name = %s 
+ORDER BY parent_id ASC, nxt_id DESC""", (set_name,))
         defer.returnValue((lset[0], layers))
     else:
         defer.returnValue(None)
@@ -52,10 +58,12 @@ ORDER BY parent_id ASC, ord DESC""", (set_name,))
 def getLayerData(dbpool, set, layers):
     """Return Deferred for layers' information fetched from database."""
     layerData = dbpool.runQuery(
-"""SELECT layertree.name, layers.name, servers.url, servers.id, servers.login, servers.passwd
-  FROM layertree JOIN layerset ON layertree.lset_id = layerset.id
-    LEFT JOIN layers ON layertree.layer_id = layers.id
-    LEFT JOIN servers ON layers.server_id = servers.id
-  WHERE layerset.name = %s AND layertree.name = ANY(%s)""", (set, layers))
+"""SELECT editor_namedlayertree.name, editor_namedlayer.name, editor_server.url, editor_server.id, editor_server.login, editor_server.passwd
+  FROM editor_layertree JOIN editor_set ON editor_layertree.lset_id = editor_set.id
+    LEFT JOIN editor_namedlayertree ON editor_namedlayertree.id = editor_layertree.named_id
+    LEFT JOIN editor_layer ON editor_layertree.layer_id = editor_layer.id
+    LEFT JOIN editor_namedlayer ON editor_namedlayer.id = editor_layer.named_id
+    LEFT JOIN editor_server ON editor_layer.server_id = editor_server.id
+  WHERE editor_set.name = %s AND editor_namedlayertree.name = ANY(%s)""", (set, layers))
     return layerData
     
